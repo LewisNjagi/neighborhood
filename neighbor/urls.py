56313bc7d3cd4c67f24aaa8bd2a2c5207f58689b
Lewis Njagi <lewismaybach@gmail.com>
2021-04-13 01:55:40 +0300

    add Api profile url path

--- neighbor/urls.py
+++ neighbor/urls.py
@@ -9,6 +9,6 @@ urlpatterns=[
     path('results/',views.search_profile,name = 'results'),
     path('join_hood/<id>',views.join,name='join'),
     path('leave_hood/<id>',views.leave,name='leave'),
-#     path('api/profile/', views.ProfileList.as_view()),
+    path('api/profile/', views.ProfileList.as_view()),
 #     path('api/users/', views.UserList.as_view()),
 ]
