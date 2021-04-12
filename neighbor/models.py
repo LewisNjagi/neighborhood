6c96113721ee41c37a8e11161d52c9cd5f6d4190
Lewis Njagi <lewismaybach@gmail.com>
2021-04-13 02:07:43 +0300

    add posts class

--- neighbor/models.py
+++ neighbor/models.py
@@ -94,12 +94,12 @@ class Business(models.Model):
     def search_business(cls, name):
         return cls.objects.filter(name__icontains=name).all()
 
-# class Post(models.Model):
-#     title = models.CharField(max_length=120, null=True)
-#     post = models.TextField()
-#     date = models.DateTimeField(auto_now_add=True)
-#     user = models.ForeignKey('Profile', on_delete=models.CASCADE, related_name='post_owner')
-#     hood = models.ForeignKey('NeighborHood', on_delete=models.CASCADE, related_name='hood_post')
+class Post(models.Model):
+    title = models.CharField(max_length=120, null=True)
+    post = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey('Profile', on_delete=models.CASCADE, related_name='post_owner')
+    hood = models.ForeignKey('NeighborHood', on_delete=models.CASCADE, related_name='hood_post')
 
 # # class NeighbourHood(models.Model):
 # #     name = models.CharField(max_length=50)
