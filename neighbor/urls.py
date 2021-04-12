2e5c6f518bf0e26a1922a457f7cfa36a68e89fa7
Lewis Njagi <lewismaybach@gmail.com>
2021-04-13 01:53:56 +0300

    add all hoods url path

--- neighbor/urls.py
+++ neighbor/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns=[
     path('',views.index,name = 'index'),
     path('profile/<str:username>/',views.profile,name = 'profile'),
-#     path('hoods/',views.hoods,name = 'hoods'),
+    path('hoods/',views.hoods,name = 'hoods'),
 #     path('single_hood/<hood_id>',views.single_hood,name = 'single_hood'),
 #     path('results/',views.search_profile,name = 'results'),
 #     path('join_hood/<id>',views.join,name='join'),
